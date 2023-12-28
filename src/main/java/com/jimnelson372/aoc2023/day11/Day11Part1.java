a1d9ce716272abc1dcee7ae2a40d80df64f12839
James Nelson <jimnelsonmail@gmail.com>
2023-12-28 15:25:58 -0600

    Day 11 cleanup files.

--- src/main/java/com/jimnelson372/aoc2023/day11/Day11Part1.java
+++ src/main/java/com/jimnelson372/aoc2023/day11/Day11Part1.java
@@ -11,16 +11,22 @@ import java.util.stream.IntStream;
 
 public class Day11Part1 {
 
-    record Coord(int x, int y) {
+    record Coord(
+            int x,
+            int y) {
 
     }
+
     public static void main(String[] args) {
         var startTime = System.nanoTime();
-        String resourcesPath = Paths.get("src", "main", "resources").toString();
-        try(BufferedReader br = Files.newBufferedReader(Paths.get(resourcesPath,"day11-puzzle-input.txt"))) {
-            var initialSpaceMap = br.lines().toList();
+        String resourcesPath = Paths.get("src", "main", "resources")
+                .toString();
+        try (BufferedReader br = Files.newBufferedReader(Paths.get(resourcesPath, "day11-puzzle-input.txt"))) {
+            var initialSpaceMap = br.lines()
+                    .toList();
             int heightOfSpace = initialSpaceMap.size();
-            int widthOfSpace = initialSpaceMap.get(0).length();
+            int widthOfSpace = initialSpaceMap.get(0)
+                    .length();
 
             var initialGalaxyCoords = getListOfGalaxyCoordinates(heightOfSpace, initialSpaceMap);
 
@@ -28,9 +34,9 @@ public class Day11Part1 {
             var emptyRows = findEmptySlicesOfSpace(initialGalaxyCoords, Coord::y, heightOfSpace);
 
             var coordsAfterExpansion = expandSpace(initialGalaxyCoords, emptyColumns, emptyRows);
-            System.out.println("Total space coordinates before expansion: " + heightOfSpace*widthOfSpace);
+            System.out.println("Total space coordinates before expansion: " + heightOfSpace * widthOfSpace);
             System.out.println("Total space coordinates after expansion: "
-                    + (heightOfSpace + emptyRows.size()*2) * (widthOfSpace + emptyColumns.size()*2));
+                                       + (heightOfSpace + emptyRows.size() * 2) * (widthOfSpace + emptyColumns.size() * 2));
             System.out.println("Number of galaxies: " + initialGalaxyCoords.size());
 
 //            // debug info
@@ -48,7 +54,50 @@ public class Day11Part1 {
             System.out.print("The puzzle input was not found at expected location.");
         }
         System.out.println("---------------");
-        System.out.println("Completed In: " +(System.nanoTime() - startTime)/ 1_000_000 + "ms");
+        System.out.println("Completed In: " + (System.nanoTime() - startTime) / 1_000_000 + "ms");
+    }
+
+    private static List<Coord> getListOfGalaxyCoordinates(int heightOfSpace, List<String> initialSpaceMap) {
+        return IntStream.range(0, heightOfSpace)
+                .boxed()
+                .<Coord>mapMulti((y, consumer) -> {
+                    var line = initialSpaceMap.get(y);
+                    var x = line.indexOf("#");
+                    while (x >= 0) {
+                        consumer.accept(new Coord(x, y));
+                        x = line.indexOf("#", x + 1);
+                    }
+                })
+                .toList();
+    }
+
+    private static List<Integer> findEmptySlicesOfSpace(List<Coord> positions, Function<Coord, Integer> xySelector,
+                                                        int totalSize) {
+        var nonEmptyColumns = positions.stream()
+                .collect(Collectors.groupingBy(xySelector, Collectors.counting()));
+        //System.out.println(nonEmptyColumns);
+        return IntStream.range(0, totalSize)
+                .filter(i -> !nonEmptyColumns.containsKey(i))
+                .boxed()
+                .toList();
+    }
+
+    private static List<Coord> expandSpace(List<Coord> positions, List<Integer> emptyColumns, List<Integer> emptyRows) {
+        return positions.stream()
+                .map(c -> {
+                    var x = c.x + emptyColumns.stream()
+                            .reduce(0,
+                                    (acc, p) -> acc + (c.x > p
+                                            ? 1
+                                            : 0));
+                    var y = c.y + emptyRows.stream()
+                            .reduce(0,
+                                    (acc, p) -> acc + (c.y > p
+                                            ? 1
+                                            : 0));
+                    return new Coord(x, y);
+                })
+                .toList();
     }
 
     private static int calculateTotalSumOfShortestPaths(List<Coord> galaxies) {
@@ -58,45 +107,16 @@ public class Day11Part1 {
                         sumOfShortestPathsFrom(galaxies.get(ndx), galaxies.subList(ndx + 1, listSize)));
     }
 
+    private static int sumOfShortestPathsFrom(Coord base, List<Coord> remList) {
+        // Math.addExact to be alerted of possible int overflow exceptions.
+        return remList.stream()
+                .map(c -> getShortestPath(base, c))
+                .reduce(0, Math::addExact);
+    }
+
     private static int getShortestPath(Coord c1, Coord c2) {
         return Math.abs(c2.x - c1.x) + Math.abs(c2.y - c1.y);
     }
 
-    private static int sumOfShortestPathsFrom(Coord base, List<Coord> remList) {
-        // Math.addExact to be alerted of possible int overflow exceptions.
-        return remList.stream().reduce(0, (acc,c) -> Math.addExact(acc,getShortestPath(base, c)),(a,b) -> 0);
-    }
-
-    private static List<Coord> expandSpace(List<Coord> positions, List<Integer> emptyColumns, List<Integer> emptyRows) {
-        return positions.stream()
-                .map(c -> {
-                    var x = c.x + emptyColumns.stream()
-                                        .reduce(0, (acc, p) -> acc + (c.x > p ? 1 : 0));
-                    var y = c.y + emptyRows.stream()
-                                        .reduce(0, (acc, p) -> acc + (c.y > p ? 1 : 0));
-                    return new Coord(x,y);
-                })
-                .toList();
-    }
-
-    private static List<Integer> findEmptySlicesOfSpace(List<Coord> positions, Function<Coord, Integer> xySelector, int totalSize) {
-        var nonEmptyColumns = positions.stream().collect(Collectors.groupingBy(xySelector,Collectors.counting()));
-        //System.out.println(nonEmptyColumns);
-        return IntStream.range(0, totalSize)
-                .filter(i -> !nonEmptyColumns.containsKey(i))
-                .boxed().toList();
-    }
-
-    private static List<Coord> getListOfGalaxyCoordinates(int heightOfSpace, List<String> initialSpaceMap) {
-        return IntStream.range(0, heightOfSpace).boxed()
-                .<Coord>mapMulti((y, consumer) -> {
-                    var line = initialSpaceMap.get(y);
-                    var x = line.indexOf("#");
-                    while(x>=0) {
-                        consumer.accept(new Coord(x,y));
-                        x = line.indexOf("#",x+1);
-                    }
-                }).toList();
-    }
 
 }
--- src/main/java/com/jimnelson372/aoc2023/day11/Day11Part2.java
+++ src/main/java/com/jimnelson372/aoc2023/day11/Day11Part2.java
@@ -12,17 +12,22 @@ import java.util.stream.LongStream;
 
 public class Day11Part2 {
 
-    record Coord(long x, long y) {
+    record Coord(
+            long x,
+            long y) {
 
     }
 
     public static void main(String[] args) {
         var startTime = System.nanoTime();
-        String resourcesPath = Paths.get("src", "main", "resources").toString();
+        String resourcesPath = Paths.get("src", "main", "resources")
+                .toString();
         try (BufferedReader br = Files.newBufferedReader(Paths.get(resourcesPath, "day11-puzzle-input.txt"))) {
-            var initialSpaceMap = br.lines().toList();
+            var initialSpaceMap = br.lines()
+                    .toList();
             int heightOfSpace = initialSpaceMap.size();
-            int widthOfSpace = initialSpaceMap.get(0).length();
+            int widthOfSpace = initialSpaceMap.get(0)
+                    .length();
 
             var initialGalaxyCoords = getListOfGalaxyCoordinates(heightOfSpace, initialSpaceMap);
 
@@ -38,7 +43,7 @@ public class Day11Part2 {
 
             System.out.println("Total space coordinates before expansion: " + heightOfSpace * widthOfSpace);
             System.out.println("Total space coordinates after expansion: "
-                    + (heightOfSpace + emptyRows.size() * expansionFactor)
+                                       + (heightOfSpace + emptyRows.size() * expansionFactor)
                     * (widthOfSpace + emptyColumns.size() * expansionFactor));
             System.out.println("Number of galaxies: " + initialGalaxyCoords.size());
 
@@ -60,50 +65,9 @@ public class Day11Part2 {
         System.out.println("Completed In: " + (System.nanoTime() - startTime) / 1_000_000 + "ms");
     }
 
-    private static long calculateTotalSumOfShortestPaths(List<Coord> galaxies) {
-        int listSize = galaxies.size();
-        return LongStream.range(0, listSize - 1)
-                .reduce(0L, (acc, ndx) -> acc +
-                        sumOfShortestPathsFrom(galaxies.get((int) ndx), galaxies.subList((int) (ndx + 1), listSize)));
-    }
-
-    private static long getShortestPath(Coord c1, Coord c2) {
-        return Math.abs(c2.x - c1.x) + Math.abs(c2.y - c1.y);
-    }
-
-    private static long sumOfShortestPathsFrom(Coord base, List<Coord> remList) {
-        // Math.addExact to be alerted of possible long overflow exceptions.
-        return remList.stream()
-                .reduce(0L,
-                        (acc, c) -> Math.addExact(acc, getShortestPath(base, c))
-                , (a, b) -> 0L);
-    }
-
-    private static List<Coord> expandSpace(List<Coord> positions, List<Long> emptyColumns, List<Long> emptyRows, long expansionFactor) {
-        return positions.stream()
-                .map(c -> {
-                    var x = c.x + emptyColumns.stream()
-                            .reduce(0L, (acc, p) -> acc + (c.x > p ? expansionFactor - 1 : 0));
-                    var y = c.y + emptyRows.stream()
-                            .reduce(0L, (acc, p) -> acc + (c.y > p ? expansionFactor - 1 : 0));
-                    return new Coord(x, y);
-                })
-                .toList();
-    }
-
-    private static List<Long> findEmptySlicesOfSpace(List<Coord> positions, Function<Coord, Long> xySelector, int totalSize) {
-        var nonEmptyColumns = positions.stream()
-                .collect(Collectors
-                        .groupingBy(xySelector,
-                                Collectors.counting()));
-        //System.out.println(nonEmptyColumns);
-        return LongStream.range(0, totalSize)
-                .filter(i -> !nonEmptyColumns.containsKey(i))
-                .boxed().toList();
-    }
-
     private static List<Coord> getListOfGalaxyCoordinates(int heightOfSpace, List<String> initialSpaceMap) {
-        return IntStream.range(0, heightOfSpace).boxed()
+        return IntStream.range(0, heightOfSpace)
+                .boxed()
                 .<Coord>mapMulti((y, consumer) -> {
                     var line = initialSpaceMap.get(y);
                     var x = line.indexOf("#");
@@ -112,7 +76,61 @@ public class Day11Part2 {
 
                         x = line.indexOf("#", x + 1);
                     }
-                }).toList();
+                })
+                .toList();
+    }
+
+    private static List<Long> findEmptySlicesOfSpace(List<Coord> positions,
+                                                     Function<Coord, Long> xySelector,
+                                                     int totalSize) {
+        var nonEmptyColumns = positions.stream()
+                .collect(Collectors
+                                 .groupingBy(xySelector,
+                                             Collectors.counting()));
+        //System.out.println(nonEmptyColumns);
+        return LongStream.range(0, totalSize)
+                .filter(i -> !nonEmptyColumns.containsKey(i))
+                .boxed()
+                .toList();
+    }
+
+    private static List<Coord> expandSpace(List<Coord> positions,
+                                           List<Long> emptyColumns,
+                                           List<Long> emptyRows,
+                                           long expansionFactor) {
+        return positions.stream()
+                .map(c -> {
+                    var x = c.x + emptyColumns.stream()
+                            .reduce(0L,
+                                    (acc, p) -> acc + (c.x > p
+                                            ? expansionFactor - 1
+                                            : 0));
+                    var y = c.y + emptyRows.stream()
+                            .reduce(0L,
+                                    (acc, p) -> acc + (c.y > p
+                                            ? expansionFactor - 1
+                                            : 0));
+                    return new Coord(x, y);
+                })
+                .toList();
+    }
+
+    private static long calculateTotalSumOfShortestPaths(List<Coord> galaxies) {
+        int listSize = galaxies.size();
+        return LongStream.range(0, listSize - 1)
+                .reduce(0L, (acc, ndx) -> acc +
+                        sumOfShortestPathsFrom(galaxies.get((int) ndx), galaxies.subList((int) (ndx + 1), listSize)));
+    }
+
+    private static long sumOfShortestPathsFrom(Coord base, List<Coord> remList) {
+        // Math.addExact to be alerted of possible long overflow exceptions.
+        return remList.stream()
+                .map(c -> getShortestPath(base, c))
+                .reduce(0L, Math::addExact);
+    }
+
+    private static long getShortestPath(Coord c1, Coord c2) {
+        return Math.abs(c2.x - c1.x) + Math.abs(c2.y - c1.y);
     }
 
 }
